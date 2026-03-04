2d49be25d2d0e384baffcd3cbd26fd666f6a5fb5
etsibareva <etsibareva.university@gmail.com>
2026-03-04 21:30:27 +0300

    commit0: init

---
+++ include/task.h
@@ -0,0 +1,8 @@
+// Copyright 2022 UNN-CS
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+
+double calculateEarthGap();
+double calculatePoolCost();
+
+#endif  // INCLUDE_TASKS_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,38 @@
 // Copyright 2022 UNN-CS
-#include <cstdint>
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include "circle.h"
 
+Circle::Circle(double r) {
+    setRadius(r);
+}
+
+void Circle::setRadius(double r) {
+    radius = r;
+    ference = 2 * M_PI * radius;
+    area = M_PI * radius * radius;
+}
+
+void Circle::setFerence(double f) {
+    ference = f;
+    radius = ference / (2 * M_PI);
+    area = M_PI * radius * radius;
+}
+
+void Circle::setArea(double a) {
+    area = a;
+    radius = std::sqrt(area / M_PI);
+    ference = 2 * M_PI * radius;
+}
+
+double Circle::getRadius() const {
+    return radius;
+}
+
+double Circle::getFerence() const {
+    return ference;
+}
+
+double Circle::getArea() const {
+    return area;
+}
